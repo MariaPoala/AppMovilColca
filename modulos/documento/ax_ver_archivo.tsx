38f4fa2a880a21295c7951f858cf8909538af7be
MariaVilcapoma <i2011451@continental.edu.pe>
2022-08-17 10:22:18 -0500

    cambios

--- modulos/documento/ax_ver_archivo.tsx
+++ modulos/documento/ax_ver_archivo.tsx
@@ -69,13 +69,13 @@ export default function AxSolicitudArchivo({ ID }: any) {
         <>
             <div className={isDownload ? "animate-pulse" : "" + " flex h-full flex-col  bg-white shadow-xl "}>
                 {/*PORTADA*/}
-                <div className="h-1 mt-1 bg-indigo-700 rounded-sm" />
+                <div className="h-1 mt-1 bg-indigo-700 rounded-sm text-slate-700 italic" />
                 {/*FORMULARIO*/}
                 <button type="button" className=" border-indigo-300 rounded-xs bg-indigo-100">Descargar</button>
                 <div className="px-0 py-0  ">
                     <div className="p-4 md:p-2">
                         {isDownload ?
-                            < svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-indigo-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            < svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-indigo-500 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                 <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"></circle>
                                 <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                             </svg> :
